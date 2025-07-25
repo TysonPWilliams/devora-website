e86c765206adbdd356f7fbda66cdcbf760b644a9
TysonPWilliams <tyson.williams95@gmail.com>
2025-07-25 15:27:53 +1000

    feat: added gif animation

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom"
+import devoraLogo from "../devora-logo.gif"
 
 function Hero() {
 
     return (
-        <section className="hero slide-in-bottom">
-            <h1 className="hero-text">Built for Results.<br/> Designed to Impress</h1>
-            <Link to="/work" className="hero-btn">see our work</Link>
+        <section className="hero-container">
+            <div className="hero slide-in-bottom">
+                <h1 className="hero-text">Built for Results.<br /> Designed to Impress</h1>
+                <Link to="/work" className="hero-btn">see our work</Link>
+            </div>
+            <div className="hero-logo">
+                <img src={devoraLogo} alt="" />
+            </div>
         </section>
     )
 }
